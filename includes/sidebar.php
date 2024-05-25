da3b8c5a01ab0a06df0de912b94f2fb252c6a1f4
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-05-25 21:20:16 +0100

    sidebar adjusted

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -46,8 +46,5 @@
                     <!-- /.row -->
                 </div>
 
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+<!-- Side Widget Well -->
+<?php include "includes/widget.php";?>
