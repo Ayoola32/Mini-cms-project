e4130a685f48be74904f83baaf8c801262b3d152
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-05-26 13:16:44 +0100

    created a resuable file for admin, seperating the header, footerr, and navigation file

---
+++ admin/includes/admin_navigation.php
@@ -0,0 +1,75 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Admin</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+            <li><a href="../index.php"><i class="fa fa-fw fa-home"></i> Home Page</a></li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+
+                    <li> <a href="#"><i class="fa fa-fw fa-table"></i> Categories</a></li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#post"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="post" class="collapse">
+                            <li>
+                                <a href="#">View all posts</a>
+                            </li>
+                            <li>
+                                <a href="#">Add new post</a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+                    <li> <a href="#"><i class="fa fa-fw fa-comment"></i> Comments</a></li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#user"><i class="fa fa-fw fa-user"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="user" class="collapse">
+                            <li>
+                                <a href="">List of Users</a>
+                            </li>
+                            <li>
+                                <a href="">Add User</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li> <a href="#">Profile</a></li>
+             
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
