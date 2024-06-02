8b62b10767ace24ce42ffb7d73614bd9677ea587
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-02 23:11:21 +0100

    display comments in post comment page

--- post_comment.php
+++ post_comment.php
@@ -100,20 +100,37 @@
 
                 <hr>
 
+               
                 <!-- Posted Comments -->
+                <?php
+                    $query = "SELECT * FROM comments WHERE comment_post_id = {$post_id_get} ORDER BY comment_id DESC";
+                    $comment_display_result= mysqli_query($connection, $query);
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
+                    if (!$comment_display_result) {
+                        die("Query Failed" . mysqli_error($connection));
+                    }
+
+                    while ($row = mysqli_fetch_assoc($comment_display_result)) {
+                        $comment_author = $row['comment_author'];
+                        $comment_content = $row['comment_content'];
+                        $comment_date = $row['comment_date'];
+                ?>
+
+                        <!-- Comment -->
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading"><?php echo $comment_author;?>
+                                    <small><?php echo $comment_date;?></small>
+                                </h4>
+                                <?php echo $comment_content;?>
+                            </div>
+
+                        </div>
+                <?php }?>
+
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
