6bbe70ad67199120d0b2c2b8815bb329932b99a7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-02 22:56:58 +0100

    query to add comment to the database(prepapre statement) and added obstart to header page to allow page to buffer well

--- post_comment.php
+++ post_comment.php
@@ -12,38 +12,38 @@
                 <?php
                     if (isset($_GET['p_id'])) {
                         $post_id_get = $_GET['p_id'];
-                    }
-                    $query = "SELECT * FROM posts WHERE post_id = '{$post_id_get}' LIMIT 1";
-                    $query_post_result = mysqli_query($connection, $query);
-                    if (!$query_post_result) {
-                        die("Query Failed" . mysqli_error($connection));
-                    }
+                    
+                        $query = "SELECT * FROM posts WHERE post_id = $post_id_get";
+                        $query_post_result = mysqli_query($connection, $query);
+                        if (!$query_post_result) {
+                            die("Query Failed" . mysqli_error($connection));
+                        }
 
-                    if (mysqli_num_rows($query_post_result)>0) {
-                        while ($row = mysqli_fetch_assoc($query_post_result)) {
-                            $post_id = $row['post_id'];
-                            $post_title = $row['post_title'];
-                            $post_author = $row['post_author'];
-                            $post_date = $row['post_date'];
-                            $post_image = $row['post_image'];
-                            $post_content = $row['post_content'];
-                ?>
-                    <!-- Display each post -->
-                    <h1 class="page-header">
-                        Page Heading
-                        <small>Secondary Text</small>
-                    </h1>
-                    <h2><a href=""><?php echo $post_title ?></a></h2>
-                    <p class="lead">by <a href=""><?php echo $post_author ?></a></p>
-                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                    <a href=""><img class="img-responsive" src="images/<?php echo $post_image?>" alt=""></a><br>
-                    <p><?php echo $post_content ?></p>
-                    <hr>
-
+                        if (mysqli_num_rows($query_post_result)>0) {
+                            while ($row = mysqli_fetch_assoc($query_post_result)) {
+                                $post_id = $row['post_id'];
+                                $post_title = $row['post_title'];
+                                $post_author = $row['post_author'];
+                                $post_date = $row['post_date'];
+                                $post_image = $row['post_image'];
+                                $post_content = $row['post_content'];
+                    ?>
+                                <!-- Display each post -->
+                                <h1 class="page-header">
+                                    Page Heading
+                                    <small>Secondary Text</small>
+                                </h1>
+                                <h2><a href=""><?php echo $post_title ?></a></h2>
+                                <p class="lead">by <a href=""><?php echo $post_author ?></a></p>
+                                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                                <a href=""><img class="img-responsive" src="images/<?php echo $post_image?>" alt=""></a><br>
+                                <p><?php echo $post_content ?></p>
+                                <hr>
                 <?php
+                            }
                         }
                     }else{
-                        echo "<h1 class='text-center'>No post Available</h1>";  // Display message if no posts are found
+                        header("Location: index.php");
                     }
                     
                 ?>
@@ -56,6 +56,30 @@
 
                 <!-- Comments Form -->
                 <div class="well">
+                    <?php
+                        if (isset($_POST['create_comment'])) {
+                            $comment_author = $_POST['comment_author'];
+                            $comment_email = $_POST['comment_email'];
+                            $comment_content = $_POST['comment_content'];
+                            
+                            if (empty($comment_author || $comment_email || $comment_content)) {
+                                echo "<h3 class=''>Field can't be empty</h3>";
+                            }else{
+                                $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                                $query.= "VALUES (?, ?, ?, ?, 'Unapproved', NOW())";
+                                $query_result = mysqli_prepare($connection, $query);
+                                
+                                mysqli_stmt_bind_param($query_result, "isss", $post_id_get, $comment_author, $comment_email, $comment_content);
+                                if (!mysqli_stmt_execute($query_result)) {
+                                    die("Execute failed: " . mysqli_stmt_error($query_result));
+                                }
+                                mysqli_stmt_close($query_result);
+                                mysqli_close($connection);
+                                header("Location: post_comment.php?p_id=$post_id_get");
+                            }
+                        }
+                    ?>
+
                     <h4>Leave a Comment:</h4>
                     <form action="" method="post" role="form">
                         <div class="form-group">
