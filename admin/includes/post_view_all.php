667c1b30cfc63c1cfa73d786e946b248b46722f1
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-03 11:41:47 +0100

    adjusted view post link from view all post page - to direct to the post comment page displaying only the requested post

--- admin/includes/post_view_all.php
+++ admin/includes/post_view_all.php
@@ -62,7 +62,7 @@
                             echo "<td>$post_status</td>";
                             echo "<td>$post_comment_count</td>";
                             echo "<td>$post_users</td>";
-                            echo "<td><a href=''>View Post</a></td>";
+                            echo "<td><a href='../post_comment.php?p_id={$post_id}'>View Post</a></td>";
                             echo "<td><a href='./posts.php?source=post_update&p_id={$post_id}'>Edit</a></td>";
                             echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this Post')\" href='./posts.php?source=post_delete&p_id={$post_id}'>Delete</a></td>";
                         }
