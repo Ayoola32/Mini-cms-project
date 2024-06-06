204e696d7560adf85e3b3ab4953545d4b21f1048
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-06 23:17:10 +0100

    dynamically changing the user role in view all user page without going to update page

--- admin/includes/user_view_all.php
+++ admin/includes/user_view_all.php
@@ -1,3 +1,19 @@
+<?php
+// for USER ROLE update
+if (isset($_POST['role']) && isset($_POST['user_id'])) {
+    $role = $_POST['role'];
+    $user_id = $_POST['user_id'];
+
+    $query = "UPDATE users SET user_role = '{$role}' WHERE user_id = {$user_id}";
+    $query_result = mysqli_query($connection, $query);
+    if (!$query_result) {
+        die("Query Failed: " . mysqli_error($connection));
+    }
+}
+?>
+
+
+
 <table class="table table-bordered hovered table-hover">
     <thead>
         <tr>
@@ -31,7 +47,26 @@
                 echo "<td>{$row['user_firstname']}</td>";
                 echo "<td>{$row['user_lastname']}</td>";
                 echo "<td>{$row['user_email']}</td>";
-                echo "<td>{$row['user_role']}</td>";
+
+            ?>  
+            <td>
+                <form action="" method="post">
+                    <select name="role" onchange="this.form.submit()">
+                        <option value='<?php echo $row['user_role']; ?>'><?php echo ucwords($row['user_role']); ?></option>
+                        <?php
+                        if ($row['user_role'] == 'admin') {
+                            echo "<option value='Subscriber'>Subscriber</option>";
+                        } else {
+                            echo "<option value='admin'>Admin</option>";
+                        }
+                        ?>
+                    </select>
+                    <input type="hidden" name="user_id" value="<?php echo $row['user_id']; ?>">
+                </form>
+            </td>
+
+            <?php
+                // echo "<td>{$row['user_role']}</td>";
                 echo "<td><img class='' width='50' src='../images/{$row['user_image']}' alt='image'></td>";
                 echo "<td><a href=''>Edit</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this Users Account?')\" href='users.php?source=user_delete&user_id={$row['user_id']}'>Delete</a></td>";
