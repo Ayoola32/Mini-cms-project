9628d398ecdc726431f9a9913c860ab7f95cfd69
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-06 22:18:42 +0100

    user add page validation added

--- admin/includes/user_add.php
+++ admin/includes/user_add.php
@@ -1,3 +1,50 @@
+<?php
+if (isset($_POST['submit'])) {
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['password'];
+    $user_role = $_POST['user_role'];
+    
+
+    // Image file handling
+    $user_image = $_FILES['user_image']['name'];
+    $user_image_temp = $_FILES["user_image"]['tmp_name'];
+    move_uploaded_file($user_image_temp, "../images/$user_image");
+
+
+    //Advance Hash Password
+    $randSalt = "alwayskeepchaseYOURgreatness2024tilleternity";
+    $user_password = password_hash($user_password, PASSWORD_BCRYPT, array($randSalt => 10 ));
+
+
+    // FORM VALIDATION USING PREPARE STATMENT
+    if (empty($user_firstname &&  $user_lastname && $username && $user_email && $user_password && $user_image)) {
+        echo "<h3>Field can't be empty</h3>";
+    }else{
+        $query = "INSERT INTO users(user_firstname, user_lastname, username, user_email, `password`, user_role, user_image, user_date_created) ";
+        $query .= "VALUE (?, ?, ?, ?, ?, ?, ?, NOW())";
+        $query_result = mysqli_prepare($connection, $query);
+        mysqli_stmt_bind_param($query_result, "sssssss", $user_firstname, $user_lastname, $username, $user_email, $user_password, $user_role, $user_image);
+
+        if (!mysqli_stmt_execute($query_result)) {
+            die("Execute failed: " . mysqli_stmt_error($query_result));
+        }else {
+            echo "<h4>User Added Successfully <a class='bg bg-success' href='users.php' style='padding: 5px 10px; margin-left: 10px;'>View All Users</a></h4>";
+        }
+        mysqli_stmt_close($query_result);
+        mysqli_close($connection);
+
+    }
+
+
+
+
+}
+
+?>
+
 <div class="col-xs-4">
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
