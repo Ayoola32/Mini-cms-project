022c2ea3eae6a096377afe396b2d2b2cc35bbe52
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-06 23:05:36 +0100

    reducee the line of codes in users viewe all page

--- admin/includes/user_view_all.php
+++ admin/includes/user_view_all.php
@@ -23,26 +23,18 @@
             }
 
             while ($row = mysqli_fetch_assoc($query_result)) {
-                $user_id = $row['user_id'];
-                $username = $row['username'];
-                $password = $row['password'];
-                $user_firstname = $row['user_firstname'];
-                $user_lastname = $row['user_lastname'];
-                $user_email = $row['user_email'];
-                $user_image = $row['user_image'];
-                $user_role = $row['user_role'];
 
                 echo "<tr>";
-                echo "<td>{$user_id}</td>";
-                echo "<td>{$username}</td>";
-                echo "<td>{$password}</td>";
-                echo "<td>{$user_firstname}</td>";
-                echo "<td>{$user_lastname}</td>";
-                echo "<td>{$user_email}</td>";
-                echo "<td>{$user_role}</td>";
-                echo "<td><img class= '' width= '50' src='../images/$user_image' alt='image'></td>";
+                echo "<td>{$row['user_id']}</td>";
+                echo "<td>{$row['username']}</td>";
+                echo "<td>{$row['password']}</td>";
+                echo "<td>{$row['user_firstname']}</td>";
+                echo "<td>{$row['user_lastname']}</td>";
+                echo "<td>{$row['user_email']}</td>";
+                echo "<td>{$row['user_role']}</td>";
+                echo "<td><img class='' width='50' src='../images/{$row['user_image']}' alt='image'></td>";
                 echo "<td><a href=''>Edit</a></td>";
-                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this Users Account?')\" href='users.php?source=user_delete&user_id={$user_id}'>Delete</a></td>";
+                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this Users Account?')\" href='users.php?source=user_delete&user_id={$row['user_id']}'>Delete</a></td>";
                 echo "</td>";
             } 
         
