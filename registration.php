4b3c5437941bd707b4295c13a8b8a60af178cfdb
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-11 23:31:15 +0100

    query to creatte new users added with some error message to display if users has been ttaken or incorrectt details and also and field empty

--- registration.php
+++ registration.php
@@ -2,6 +2,67 @@
  <?php  include "includes/header.php"; ?>
 <?php  include "includes/navigation.php"; ?>
 
+<?php
+if (isset($_POST["submit"])) {
+    $username = $_POST['username'];
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+
+    // sanitize input
+    $username = mysqli_real_escape_string($connection, $username);
+    $email    = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection,$password);
+   
+    // Hash Password
+    $randSalt = "alwayskeepchaseYOURgreatness2024tilleternity";
+    // $password = crypt($password, $randSalt); 
+
+    //Advance hashpassworrd
+    $password = password_hash($password, PASSWORD_BCRYPT, array($randSalt => 12 ));
+
+    // fetching usernames from database to confirm with new one
+    $query = "SELECT username FROM users WHERE username = '{$username}'";
+    $query_db_username = mysqli_query($connection, $query);
+    if (!$query_db_username) {
+        die("Query Failed:" . mysqli_error($connection));
+    }
+
+    // fetching email from database to confirm with new one
+    $query = "SELECT user_email FROM users WHERE user_email = '{$email}'";
+    $query_db_email = mysqli_query($connection, $query);
+    if (!$query_db_email) {
+        die("Query Failed:" . mysqli_error($connection));
+    }
+
+
+ 
+    if (empty($username && $email && $password)) {
+        $message = "<h4 class='text-center alert alert-danger' role='alert'>Field can't be empty</h4>";
+    }elseif (mysqli_num_rows($query_db_username)>0 || mysqli_num_rows($query_db_email)>0) {
+        $message = "<h4 class='alert alert-warning text-center'>Details has been taken</h4>";
+    }else{
+    // Using prepared statements to prevent SQL injection
+    $query = "INSERT INTO users (username, `password`, user_firstname, user_lastname, user_email, user_image, user_role, user_date_created) ";
+    $query.= "VALUES (?, ?, '', '', ?, '', 'subscriber', NOW())";
+    $query_result = mysqli_prepare($connection, $query);
+   
+    mysqli_stmt_bind_param($query_result, "sss", $username, $password, $email);
+    
+    if (!mysqli_stmt_execute($query_result)) {
+        die("Execute failed: " . mysqli_stmt_error($query_result));
+    }
+    $message = "<h4 class='text-center alert alert-success'>Register Successfully" . " " . "<a class='bg-success' href=./index.php>Login</a></h4>";
+    // mysqli_stmt_close($query_result);
+    // mysqli_close($connection);
+    // header("Location: ./users.php");
+    }
+    
+}else {
+    $message="";
+}
+
+?>
+
 
 <!-- Page Content -->
 <div class="container">
@@ -12,7 +73,7 @@
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
                 <h1>Register New User</h1>
-                    <h6 class="text-center"></h6>
+                    <h6 class="text-center"><?php echo $message;?></h6>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                         <div class="form-group">
                             <label for="username" class="sr-only">username</label>
