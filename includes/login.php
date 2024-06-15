08ebf27d81487858695fada1b159ada498e7052a
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-15 01:36:19 +0100

    add user_id session so i can fetch it from the setting url

--- includes/login.php
+++ includes/login.php
@@ -30,6 +30,7 @@ if (isset($_POST['submit'])) {
         $db_user_role = $row['user_role'];
 
         if (password_verify($password, $db_user_password)) {
+            $_SESSION['user_id'] = $db_user_id;
             $_SESSION['username'] = $db_username;
             $_SESSION['user_firstname'] = $db_user_firstname;
             $_SESSION['user_lastname'] = $db_user_lastname;
