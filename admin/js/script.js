6df2627e0e7303e86f1cb94e2c1d3b32ab3a4500
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-23 01:14:52 +0100

    jquery to bulk  select all post with the radio button

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    $('#selectAllboxes').click(function(){
+        if (this.checked) {
+            $('.checkBoxes').each(function(){
+                this.checked = true;
+            })
+        } else{
+            $('.checkBoxes').each(function(){
+                this.checked = false;
+            })
+        }
+    });
+
     $('#summernote').summernote({
         height: 200,
         focus: true
