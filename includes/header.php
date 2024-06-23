046815385c482ade99cef443f15ba26970c8301b
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-06-23 15:21:34 +0100

    style added to the index pagination

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,9 @@
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
 
+    <!-- Pagination CSS -->
+    <link rel="stylesheet" href="css/pagination_style.css">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
