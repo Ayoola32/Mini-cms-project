1a56f268f2ebda09d6a9e366fbb6193474df6370
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-07 13:38:24 +0100

    some fucntions added

--- admin/function.php
+++ admin/function.php
@@ -1,5 +1,50 @@
 <?php
 
+function redirect($location){
+
+
+    header("Location:" . $location);
+    exit;
+
+}
+
+
+function ifItIsMethod($method=null){
+
+    if($_SERVER['REQUEST_METHOD'] == strtoupper($method)){
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+function isLoggedIn(){
+
+    if(isset($_SESSION['user_role'])){
+
+        return true;
+
+
+    }
+
+
+   return false;
+
+}
+
+function checkIfUserIsLoggedInAndRedirect($redirectLocation=null){
+
+    if(isLoggedIn()){
+
+        redirect($redirectLocation);
+
+    }
+
+}
+
 
 
 // // // // // // // // // // // // // // // // // // // // 
