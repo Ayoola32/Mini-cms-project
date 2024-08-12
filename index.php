bd42dc175d45a0056dd224aaa99763e61889e758
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-12 20:26:03 +0100

    adjusted datetime display format

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                             </h1>
                             <h2><a href="post_comment.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a></h2>
                             <p class="lead">by <a href="post_comment.php?p_id=<?php echo $post_id?>"><?php echo $post_author ?></a></p>
-                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo date('F d, Y \a\t g:i A', strtotime($post_date)) ?></p>
                             <a href="post_comment.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image?>" alt=""></a>
                             <p><?php echo $post_content ?></p>
                             <a class="btn btn-primary" href="post_comment.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
